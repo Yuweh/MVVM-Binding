cbd2ea44ceb05f42915a39d6c82ffd6a08e70538
Francis Jemuel Bergonia <francisb.itcareers@gmail.com>
2018-05-17 14:40:05 +0800

    Initial Commit

---
+++ MVVMBinding/RegistrationTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  RegistrationTableViewController.swift
+//  MVVMBinding
+//
+//  Copyright © ©XiAPPS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RegistrationTableViewController : UITableViewController {
+    
+    @IBOutlet weak var emailTextField :UITextField!
+    @IBOutlet weak var passwordTextField :UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func save() {
+        
+        
+    }
+    
+}
